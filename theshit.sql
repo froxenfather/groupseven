6d00e143c7697f2cd9bb3d4a521b27ddb9e7c9ac
froxenfather <ryandfreese@icloud.com>
2025-11-14 16:00:15 -0700

    edited

--- theshit.sql
+++ theshit.sql
@@ -1,7 +1,9 @@
 CREATE TABLE bigitemtotal (
-    itemid      SERIAL PRIMARY KEY,
+    item_id      SERIAL PRIMARY KEY,
     item_name   VARCHAR(255) NOT NULL,
     store       VARCHAR(100) NOT NULL,
-    priceItem   DECIMAL(10,2) NOT NULL,
+    price_item   DECIMAL(10,2) NOT NULL,
     rating      DECIMAL(3,2)
 );
+
+
