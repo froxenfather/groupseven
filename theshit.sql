288f08274a50bb8f6c6ab0f49c40fd928785ea24
froxenfather <ryandfreese@icloud.com>
2025-11-14 15:58:38 -0700

    big table

--- theshit.sql
+++ theshit.sql
@@ -1,4 +1,7 @@
-CREATE table goons (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TABLE bigitemtotal (
+    itemid      SERIAL PRIMARY KEY,
+    item_name   VARCHAR(255) NOT NULL,
+    store       VARCHAR(100) NOT NULL,
+    priceItem   DECIMAL(10,2) NOT NULL,
+    rating      DECIMAL(3,2)
 );
